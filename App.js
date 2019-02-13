db14c0d150423bfebd78710384767fea876813ee
selimyan <gselimyan@gmail.com>
2019-02-12 22:50:21 -0500

    initial structure

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import vibrate from './utils/vibrate'
 
 export default class App extends React.Component {
+  state = {
+    workTime: 1800,
+    breakTime: 300,
+    timeRemaining: 1800,
+    isRunning: false,
+    activeTimer: 'work'
+  }
+
+  componentDidMount() {
+    vibrate()
+  }
+
   render() {
+    const { workTime, breakTime, timeRemaining, isRunning, activeTimer } = this.state
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={[styles.title, styles.center]}>{activeTimer} TIME</Text>
+        <Text style={styles.center}>Countdown Placeholder</Text>
+        <View style={[styles.buttonGroup, styles.center]}>
+          <Text>Start/Pause</Text>
+          <Text>Reset</Text>
+        </View>
+        <Text style={styles.center}>Work</Text>
+        <Text style={styles.center}>Break</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -15,7 +36,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    paddingTop: 150,
   },
-});
+  center: {
+    alignSelf: 'center',
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 48,
+    textTransform: 'uppercase',
+  },
+})
