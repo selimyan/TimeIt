ac99ad6066040a004450eee586072b01bfe76316
selimyan <gselimyan@gmail.com>
2019-02-13 12:34:53 -0500

    added methods: updateTime and resetTime

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import { StyleSheet, Text, View } from 'react-native'
 import Countdown from './components/Countdown'
 import { vibrate, Timer } from './utils'
 
+const DEFAULT_WORK_MINS = 30
+const DEFAULT_BREAK_MINS = 5
+
 const nextTimer = { work: 'break', break: 'work' }
 
 export default class App extends React.Component {
   state = {
-    workTime: 1800,
-    breakTime: 300,
-    timeRemaining: 1800 * 1000,
+    workTime: DEFAULT_WORK_MINS * 60,
+    breakTime: DEFAULT_BREAK_MINS * 60,
+    timeRemaining: DEFAULT_WORK_MINS * 60 * 1000,
     isRunning: false,
     activeTimer: 'work'
   }
@@ -27,11 +30,30 @@ export default class App extends React.Component {
     this.setState({ timeRemaining })
   }
 
+  updateTime = (target) => (time, shouldStartTimer) => {
+    if (this.state.activeTimer === target) {
+      if (this.timer) this.timer.stop()
+      const timeRemaining = +time * 1000
+      this.timer = new Timer(timeRemaining, this.updateTimeRemaining, this.handleTimerEnd)
+      if (!shouldStartTimer) this.timer.stop()
+      this.setState({ [`${target}Time`]: time, timeRemaining, isRunning: this.timer.isRunning })
+    } else {
+      this.setState({ [`${target}Time`]: time, isRunning: this.timer.isRunning })
+    }
+  }
+
+  resetTimer = shouldStopTimer => {
+    const { activeTimer } = this.state
+    this.updateTime(activeTimer)(this.state[`${activeTimer}Time`], !shouldStopTimer)
+  }
+
   handleTimerEnd = () => {
     vibrate()
     this.setState((prevState) => (
       { activeTimer: nextTimer[prevState.activeTimer] }), this.resetTimer)
   }
+
+
 
   render() {
     const { workTime, breakTime, timeRemaining, isRunning, activeTimer } = this.state
